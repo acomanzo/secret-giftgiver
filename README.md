2626b5392fc45693d9cfbe099c192f8b3052d937
acomanzo <ansonih17@gmail.com>
2020-11-16 15:40:29 -0500

    added my project

---
+++ README.md
@@ -0,0 +1,26 @@
+# Introduction
+Want to do a Secret Santa party but want to keep it inclusive? Don't have a way to randomize the givers and receivers?
+Well this repository is the repository for you! Simply follow the instructions below and all of your problems will go away. 
+
+# Prerequisites
+```brew install node```
+### Verify install 
+```node -v```
+```npm -v```
+### Getting nodemailer
+```npm install nodemailer```
+```yarn add nodemailer```
+### Let the app access your account (assuming you're using gmail)
+I'd recommend creating a burner for this application to work.
+1. [Disable the unlock captcha.](https://accounts.google.com/b/0/DisplayUnlockCaptcha)
+2. [Allow less secure apps to access your account.](https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4OE7OVU36LvIrKX5evD7WNKe4EZadHTGAbNoQF4KTnG8m9tFMw44aIngkpICkrG5j4xDfrWkhvugTfIv-ZkK292mCxJzA)
+
+# Instructions
+1. In `emails.json`, update the emails to reflect you and your compatriots' emails. Make sure the key is an email and value is a name. 
+3. In `server.json` replace the value at line 15 with your email. 
+4. In `server.json` replace the value at line 16 with your password. 
+5. In `server.json` replace the string at line 44 with whatever signature you'd like. 
+6. In `server.json` replace the value at line 48 with your email. 
+7. On a command line/terminal:
+    ```node server.js```
+    Then, in a web browser, go to localhost:8888 and watch in glory. 
---
+++ server.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const nodemailer = require('nodemailer') 
+const emails = require('./emails.json')
+
+function onRequest(request, response) {
+    console.log(`A user made a request: ${request.url}`);
+
+    // 200 code ok
+    response.writeHead(200, {'Context-Type': 'text/plain'});
+    response.write('Thanks for using the app, champ.');
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com', 
+        auth: {
+            user: 'YOUR EMAIL GOES HERE',
+            pass: 'YOUR PASSWORD GOES HERE'
+        }
+    });
+
+    // make object to hold the pairs where key is the giver and value is the receiver
+    var pairs = {};
+
+    // make object to hold people that have been picked as recipients
+    var pickedRecipients = {};
+
+    // a list of the keys aka emails
+    var keys = Object.keys(emails)
+    // go through each email, picking a random receiver that hasn't been picked yet
+    for (key in emails) {
+        var recipientEmail = keys[keys.length * Math.random() << 0];
+        while (recipientEmail in pickedRecipients || key === recipientEmail) {
+            recipientEmail = keys[keys.length * Math.random() << 0];
+        }
+        pairs[key] = recipientEmail;
+        pickedRecipients[recipientEmail] = recipientEmail;
+    }
+
+    // send the emails
+    for (key in pairs) {
+        const body = 
+        '<div style="color: yellow; text-align: center;">' + 
+            '<h1>Hey there, sport.</h1>' +
+            '<p>You get to find a gift for <b>' + emails[pairs[key]] + '</b>.</p>' + 
+            '<p>Yours in heart,<br>YOUR FANCY NAME HERE</p>' +
+        '</div>';
+
+        const mailOptions = {
+            from: 'YOUR EMAIL GOES HERE',
+            to: key, 
+            subject: 'Secret Non-Denominational Gift Giving Boogaloo',
+            html: body 
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(`Email sent to ${key}: ${info.response}`);
+            }
+        });
+    }
+
+    response.end();
+}
+
+// listen on port 8080
+http.createServer(onRequest).listen(8888);
+console.log('Server started...')
